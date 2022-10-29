646d9c70188165db05c1c60718ed4b6f9b6e3ac5
Jess John <jessjohn1539@gmail.com>
2022-10-29 23:55:52 +0530

    modified:   .vscode/index.html 	modified:   .vscode/responses.js

--- .vscode/responses.js
+++ .vscode/responses.js
@@ -1,14 +1,5 @@
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
-
-    // Simple responses
+     // Simple responses
     if (input == "hello") {
         return "Hello there!";
     } else if (input == "goodbye") {
