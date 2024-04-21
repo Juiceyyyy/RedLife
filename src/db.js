6847dd9ac78903774e8bb039a68ab1c380e571a6
Joshua Menezes <112465506+Juiceyyyy@users.noreply.github.com>
2024-04-21 16:22:55 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Joshua*18',
+    user: 'your_username',
+    password: 'your_password',
 });
 
 const createDatabase = () => {
